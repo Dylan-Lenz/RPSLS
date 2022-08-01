ce736cb4159562ef8f62926e5029c96d5a384595
unknown <dylanjlenz@gmail.com>
2022-08-01 10:31:12 -0500

    Finish Player Class, Create Ai Mod

--- player.py
+++ player.py
@@ -1,8 +1,10 @@
 from unicodedata import name
 
 
-def Player(self):
-    self.name = name
-    self.score = 0
-    self.gestures = []
-    self.chosen_gesture = 'Rock'
+class Player:
+
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+        self.gestures = []
+        self.chosen_gesture = 'Rock'
