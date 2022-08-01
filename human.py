489d5496bcb247694115516f183653b7fd23c599
unknown <dylanjlenz@gmail.com>
2022-08-01 11:30:46 -0500

    Create Gesture Choice in Human Class

--- human.py
+++ human.py
@@ -8,3 +8,10 @@ class Human(Player):
         super().__init__()
         self.score = 0
         self.name = name
+
+    def gesture_choice(self):
+        user_input = input('/nPlease make your selection: ')
+        self.set_gesture = user_input
+        gestures = ['Rock', 'Paper','Scissors', 'Lizard', 'Spock']
+        sleep(1)
+        print(f'{self.name} has chosen to throw {gestures[int(self.set_gesture)]} /n')
