be3409431b23686bcd0288e286fe479685291e4b
unknown <dylanjlenz@gmail.com>
2022-08-01 11:13:36 -0500

    Define Gesture Choice Method in AI Class

--- ai.py
+++ ai.py
@@ -8,3 +8,9 @@ class AI(Player):
         super().__init__()
         self.score = 0
         self.name = name
+
+    def gesture_choice(self):
+        self.set_gesture = str(random.randint(0,4))
+        gestures = ['Rock', 'Paper','Scissors', 'Lizard', 'Spock']
+        sleep(1)
+        print(f'{self.name} has chosen to throw {gestures[int(self.chosen_gesture)]}')
