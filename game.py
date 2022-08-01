6c8ae213640948f6ae70db6ca59262650b8ca45e
unknown <dylanjlenz@gmail.com>
2022-08-01 11:57:30 -0500

    Create Game Class in Game Module

---
+++ game.py
@@ -0,0 +1,12 @@
+from human import Human
+from time import sleep
+from ai import AI
+
+class Game():
+
+    def __init__(self):
+        super().__init__
+        self.player1 = ''
+        self.player2 = ''
+
+    
