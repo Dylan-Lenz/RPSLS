e8413149f83ee4c7a69ea7e871efabc4ff49a192
unknown <dylanjlenz@gmail.com>
2022-08-01 12:17:54 -0500

    Create Display Welcome Method in Game Class

--- game.py
+++ game.py
@@ -9,4 +9,17 @@ class Game():
         self.player1 = ''
         self.player2 = ''
 
-    
+    def display_welcome(self):
+        sleep(1)
+        print('/nWelcome to Rock-Papers-Scissors-Lizard-Spock!!/n')
+        print('Rules of the Game Are:')
+        print('Rock Crushes Paper')
+        print('Rock Crushes Scissors')
+        print('Paper Covers Rock')
+        print('Paper Disproves Spock')
+        print('Scissors Cuts Paper')
+        print('Scissors Decapitates Lizard')
+        print('Lizard Poisons Spock')
+        print('Lizard Eats Paper')
+        print('Spock Smashes Scissors')
+        print('Spock Vaporizes Rock/n')
